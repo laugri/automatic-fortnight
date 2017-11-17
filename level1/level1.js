ce9f9b63c2482cd3e70dcfe0f60bddf24aa90c8e
Laurent Grima <grima.laurent@gmail.com>
2017-11-17 16:33:25 +0100

    Add basic unit tests on level1

--- level1/level1.js
+++ level1/level1.js
@@ -1,9 +1,25 @@
 const output = require('./output.json');
+const data = require('./data.json');
+
+function getArticle(articleId) {
+  return data.articles.find(article => article.id === articleId);
+}
+
+function computeItemPrice(item) {
+  return getArticle(item.article_id) * item.quantity;
+}
+
+function computeCartPrice(cart) {
+  return cart.items.reduce((sum, item) => sum + computeItemPrice(item));
+}
 
 function buildOutput() {
-	return output;
+  return output;
 }
 
 module.exports = {
-	buildOutput,
+  buildOutput,
+  getArticle,
+  computeItemPrice,
+  computeCartPrice,
 };
--- level1/level1.test.js
+++ level1/level1.test.js
@@ -2,8 +2,31 @@ const level1 = require('./level1.js');
 const data = require('./data.json');
 const output = require('./output.json');
 
-describe('Level 1 acceptance test', () => {
-    test('Outputs carts correctly', () => {
-        expect(level1.buildOutput(data)).toEqual(output);
+describe('Level 1', () => {
+  describe('buildOutput', () => {
+    test('on data.json object outputs output.json object', () => {
+      expect(level1.buildOutput(data)).toEqual(output);
     });
+  });
+  describe('getArticle', () => {
+    test('(1) returns article with id 1', () => {
+      expect(level1.getArticle(1)).toEqual({
+        id: 1,
+        name: 'water',
+        price: 100,
+      });
+    });
+
+    test('(3) returns article with id 3', () => {
+      expect(level1.getArticle(3)).toEqual({
+        id: 3,
+        name: 'mango',
+        price: 400,
+      });
+    });
+
+    test('returns undefined for unknown id', () => {
+      expect(level1.getArticle(10)).toBe(undefined);
+    });
+  });
 });
