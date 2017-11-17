144db15777b43bd9d2ec9cb34cf8adf3bc20a886
Laurent Grima <grima.laurent@gmail.com>
2017-11-17 16:32:35 +0100

    Add eslint and editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'prettier/flowtype',
+    'prettier/react',
+    'prettier/standard',
+  ],
+};
